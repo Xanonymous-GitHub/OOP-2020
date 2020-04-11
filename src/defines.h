6ce331b401b73a936d17d2c803ae1c21171fbd87
Xanonymous <t108820003@ntut.org.tw>
2020-04-11 14:55:55 +0800

    finish kernel

--- src/defines.h
+++
@@ -1,8 +0,0 @@
-#define $v(x) std ::vector<x>
-#define show(x) std::cout << x << std::endl
-#define nl << endl
-#define add push_back
-#define str std::string
-#define $mount(x) assign(x, x + (sizeof(x) / sizeof(*x)));
-#define $speedUP ios::sync_with_stdio(0), cin.tie(0)
-#define $(x) (x).get_poly()
---
+++ src/term.h
@@ -0,0 +1,84 @@
+#define $Term(x)                 \
+    Term x(new_c, new_exponent); \
+    return x
+
+class Term
+{
+public:
+    // default constructor
+    // default constructor should construct a term //
+    // with coefficient 0 and exponent zero        //
+    Term()
+    {
+        c = 0.0;
+        exponent = 0;
+    }
+
+    // constructor
+    Term(double coefficient, int exponent)
+    {
+        c = coefficient;
+        exponent = exponent;
+    }
+
+    // operator assignment
+    Term &operator=(Term const &t)
+    {
+        c = t.c;
+        exponent = t.exponent;
+        return *this;
+    }
+
+    // operator add
+    Term operator+(Term const &t)
+    {
+        double new_c = c + t.c;
+        if (exponent != t.exponent)
+        {
+            throw "exponent not match!\n";
+        }
+        int new_exponent = exponent;
+        $Term(new_term);
+    }
+
+    // operator multiply
+    Term operator*(Term const &t)
+    {
+        double new_c = c * t.c;
+        int new_exponent = exponent + t.exponent;
+        $Term(new_term);
+    }
+
+    // operator equal
+    bool operator==(Term const &t)
+    {
+        return (c == t.c && exponent == t.exponent);
+    }
+
+    // access coefficient of a term
+    double getCoefficient() const
+    {
+        return c;
+    }
+
+    // access exponent of a term
+    int getExponent() const
+    {
+        return exponent;
+    }
+
+    // evaluate by a given x
+    double evaluate(double x)
+    {
+        int x_tmp = 1;
+        for (int i = 0; i < exponent; i++)
+        {
+            x_tmp *= x;
+        }
+        return c * x_tmp;
+    }
+
+private:
+    double c = 0.0;
+    int exponent = 0;
+};
